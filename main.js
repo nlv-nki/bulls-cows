12fc22cea403e5b3ac25f0c90d89b62c7b9940d2
Maryna Semeniuknpm <maryna_semeniuk@epam.com>
2018-09-06 03:28:55 +0300

    crear progress container: fixed

--- main.js
+++ main.js
@@ -1,44 +1,9 @@
-// let secretCode = '9305';
-// let assumption = prompt('Enter a number that includes 4 different digits', '9012') || '';
-
-// function getBullsAndCows(secretCode, assumption) {
-//   let cows = 0;
-//   let bulls = 0;
-//   let victoryMsg = 'You won! Congrats :D';
-//   let motivationMsg = 'Please, try again';
-
-//   assumption = assumption.split('');
-
-//   for (let i = 0; i < assumption.length; i++) {
-//     if (~secretCode.indexOf(assumption[i])) {
-//       cows++;
-//       if (assumption[i] == secretCode[i]) {
-//         bulls++;
-//         cows--;
-//       }
-//     }
-//   }
-
-//   console.log('You\'ve got ' + bulls + ' bull(s) and ' + cows + ' cow(s)!');
-
-//   if (bulls == 4) {
-//     return victoryMsg;
-//   }
-  
-//   return motivationMsg;
-// }
-
-// console.log(getBullsAndCows(secretCode, assumption));
-
-let secretCode = '9305';
+let secretCode = '9305'; // hardcoded value
 
 function getBullsAndCows(secretCode, assumption) {
-  clearInput('assumption');
-
   let cows = 0;
   let bulls = 0;
-  let victoryMsg = 'You won! Congrats :D';
-  let motivationMsg = 'Please, try again';
+  let victoryMsg = 'You won! Congrats :D ';
 
   assumption = assumption.split('');
 
@@ -52,18 +17,26 @@ function getBullsAndCows(secretCode, assumption) {
     }
   }
 
-  let notification = 'You\'ve got ' + bulls + ' bull(s) and ' + cows + ' cow(s)!';
+  let notification = assumption.join('') + ' ' + bulls + ' bull(s) ' + cows + ' cow(s)';
   addProgressMessage(notification);
 
   if (bulls == 4) {
-    addProgressMessage(victoryMsg);
+    clearAssumptionContainer();
+    addProgressMessage(victoryMsg, true);
   }
+
+  clearInput('assumption');
 }
 
-function addProgressMessage(str) {
+function addProgressMessage(str, victoryState) {
   let chat = document.getElementById('assumptions-list');
   let item = document.createElement('div');
   item.classList = 'assumptions-list-item';
+
+  if (victoryState) {
+    item.classList = 'assumptions-list-item assumptions-list-item-success';
+  }
+
   item.textContent = str;
   chat.appendChild(item);
 }
@@ -71,6 +44,7 @@ function addProgressMessage(str) {
 function clearAssumptionContainer() {
   let el = document.getElementById('assumptions-list');
   let fc = el.firstChild;
+
   while (fc) {
     el.removeChild(fc);
     fc = el.firstChild;
@@ -83,5 +57,18 @@ function clearInput(id) {
 }
 
 function validateInput() {
-  /**/
+  /*
+    Validation goes here (maybe some time later)
+
+    TODO:
+      ** only nubmers
+      ** only 4 digits
+      ** all different digits
+
+  */
 }
+
+/*
+  TODO: 
+  ** make dynamic secretCode
+*/
