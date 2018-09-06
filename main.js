2777fb9d73d5ce37f4d085df4398dbe661af5f1b
Nikolai <nlv.nki@gmail.com>
2018-09-06 14:07:15 +0300

    Add func numGen, validateInput

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-let secretCode = '9305'; // hardcoded value
+// let secretCode = '9305';  hardcoded value
+let secretCode = numGen().toString();
 
-function getBullsAndCows(secretCode, assumption) {
+function getBullsAndCows (secretCode, assumption) {
   let cows = 0;
   let bulls = 0;
   let victoryMsg = 'You won! Congrats :D ';
+  if (validateInput(+assumption)) {
+    assumption = assumption.split('');
 
-  assumption = assumption.split('');
-
-  for (let i = 0; i < assumption.length; i++) {
-    if (~secretCode.indexOf(assumption[i])) {
-      cows++;
-      if (assumption[i] == secretCode[i]) {
-        bulls++;
-        cows--;
+    for (let i = 0; i < assumption.length; i++) {
+      if (~secretCode.indexOf(assumption[i])) {
+        cows++;
+        if (assumption[i] == secretCode[i]) {
+          bulls++;
+          cows--;
+        }
       }
     }
+
+    let notification = assumption.join('') + ' ' + bulls + ' bull(s) ' + cows + ' cow(s)';
+    addProgressMessage(notification);
+
+    if (bulls == 4) {
+      clearAssumptionContainer();
+      addProgressMessage(victoryMsg, true);
+    }
+
+    clearInput('assumption');
+  } else {
+    alert('Ошибка: не верный формат числа')
   }
-
-  let notification = assumption.join('') + ' ' + bulls + ' bull(s) ' + cows + ' cow(s)';
-  addProgressMessage(notification);
-
-  if (bulls == 4) {
-    clearAssumptionContainer();
-    addProgressMessage(victoryMsg, true);
-  }
-
-  clearInput('assumption');
 }
 
-function addProgressMessage(str, victoryState) {
+function addProgressMessage (str, victoryState) {
   let chat = document.getElementById('assumptions-list');
   let item = document.createElement('div');
   item.classList = 'assumptions-list-item';
@@ -41,7 +45,7 @@ function addProgressMessage(str, victoryState) {
   chat.appendChild(item);
 }
 
-function clearAssumptionContainer() {
+function clearAssumptionContainer () {
   let el = document.getElementById('assumptions-list');
   let fc = el.firstChild;
 
@@ -51,12 +55,12 @@ function clearAssumptionContainer() {
   }
 }
 
-function clearInput(id) {
+function clearInput (id) {
   let userInput = document.getElementById(id);
   userInput.value = '';
 }
 
-function validateInput() {
+function validateInput (value) {
   /*
     Validation goes here (maybe some time later)
 
@@ -66,9 +70,10 @@ function validateInput() {
       ** all different digits
 
   */
+
+  return !isNaN(parseFloat(value)) && isFinite(value) && value.toString().length === 4;
 }
 
-/*
-  TODO: 
-  ** make dynamic secretCode
-*/
+function numGen () {
+  return Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+}
